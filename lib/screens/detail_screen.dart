821af18610d8188584101b8219678e958d82cbcf
Anindia Azzahra <102598538+anindiaazzahra@users.noreply.github.com>
2024-03-23 12:37:35 +0700

    fix overflow widget

--- lib/screens/detail_screen.dart
+++ lib/screens/detail_screen.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:kuis072/models/top_album.dart';
 import 'package:url_launcher/url_launcher.dart';
 
@@ -67,21 +69,25 @@ class _DetailPageState extends State<DetailPage> {
                         mainAxisAlignment: MainAxisAlignment.spaceBetween,
                         children: [
                           Icon(Icons.calendar_month_outlined),
-                          Text(
-                            widget.topAlbum.firstEntryDate,
-                            style: const TextStyle(fontSize: 16),
-                            textAlign: TextAlign.center,
+                          Expanded(
+                            child: Text(
+                              widget.topAlbum.firstEntryDate,
+                              style: const TextStyle(fontSize: 16),
+                              textAlign: TextAlign.center,
+                            ),
                           ),
                           Text(
-                            "|",
+                            "| ",
                             style: const TextStyle(fontSize: 16),
                             textAlign: TextAlign.center,
                           ),
                           Icon(Icons.home_work_outlined),
-                          Text(
-                            widget.topAlbum.source,
-                            style: const TextStyle(fontSize: 16),
-                            textAlign: TextAlign.center,
+                          Expanded(
+                            child: Text(
+                              widget.topAlbum.source,
+                              style: const TextStyle(fontSize: 16),
+                              textAlign: TextAlign.center,
+                            ),
                           ),
                         ],
                       ),
